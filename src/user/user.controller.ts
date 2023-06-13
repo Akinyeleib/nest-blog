ab1d6960a1acc49a5bb78f2ede7de76c96792e81
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-06-13 21:30:55 +0100

    Password hash implemented

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,31 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDTO } from 'src/dto/user.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Controller('user')
 export class UserController {
+
   @Post()
-  createUser(@Body() createUserDTO: CreateUserDTO) {
-    return createUserDTO;
+  async createUser(@Body() createUserDTO: CreateUserDTO) {
+
+    if (createUserDTO.password1 !== createUserDTO.password2) {
+      throw new BadRequestException("Password mismatch");
+    }
+    const password = await bcrypt.hash(createUserDTO.password1, 10)
+
+    const {first_name, last_name, email, username, security_question, security_answer} = createUserDTO;
+    
+    return {
+      first_name,
+      last_name,
+      email,
+      username,
+      password,
+      security_question,
+      security_answer
+    };
+  
   }
+
 }

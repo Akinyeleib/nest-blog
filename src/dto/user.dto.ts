283d27a9a39cbc25048b00957ce7be15170f61b0
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-06-13 20:44:06 +0100

    Create User DTO implemented

---
+++ src/dto/user.dto.ts
@@ -0,0 +1,41 @@
+/* eslint-disable prettier/prettier */
+
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+export class CreateUserDTO {
+
+
+  @IsString({message: "Please enter a valid string"})
+  @IsNotEmpty({message: "first_name cannot be blank"})
+  first_name: string;
+
+  @IsString()
+  @IsNotEmpty({message: "last_name cannot be blank"})
+  last_name: string;
+
+  @IsString()
+  @IsNotEmpty({message: "username cannot be blank"})
+  username: string;
+
+  @IsEmail()
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password1: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password2: string;
+
+  @IsString()
+  @IsNotEmpty()
+  security_question: string;
+
+  @IsString()
+  @IsNotEmpty()
+  security_answer: string;
+
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDTO } from 'src/dto/user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Post()
+  createUser(@Body() createUserDTO: CreateUserDTO) {
+    return createUserDTO;
+  }
+}

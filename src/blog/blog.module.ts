676b34dd770712ff6d68c7340394fea6c0d2e1a2
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-06-15 06:36:24 +0100

    Blog body improved

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BlogController } from './blog.controller';
 import { BlogService } from './blog.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Blog } from 'src/entity/blog.entity';
 
 @Module({
   controllers: [BlogController],
-  providers: [BlogService]
+  providers: [BlogService],
+  imports: [TypeOrmModule.forFeature([Blog])],
 })
 export class BlogModule {}
--- src/dto/blog.dto.ts
+++ src/dto/blog.dto.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 
 export class CreateBlogDTO {
-  author_id: number
+  title: string
   content: string
 }

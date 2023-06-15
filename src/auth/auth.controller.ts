920763b3ecb67fcf6419d13e93883c0077c7ff4c
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-06-15 07:08:57 +0100

    Dependency Injection for UserService implemented

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
+import { LoginDTO } from 'src/dto/login.dto';
+import { User } from 'src/entity/user.entity';
+import { UserService } from 'src/user/user.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private userService: UserService) {}
+  @Get('login')
+  async login(@Body() loginDTO: LoginDTO): Promise<User> {
+    const user = await this.userService.getUserByUsername(loginDTO.loginID);
+    console.log(user);
+    return user;
+  }
+}

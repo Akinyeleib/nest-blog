22b43c990154e4cb7add200486687c1e034391dc
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-06-15 07:04:11 +0100

    LoginDTO conditions implemented

--- src/dto/login.dto.ts
+++ src/dto/login.dto.ts
@@ -1,5 +1,13 @@
 /* eslint-disable prettier/prettier */
+
+import { IsStringNotEmpty } from 'src/utils/helper';
+
 export class LoginDTO {
+
+  @IsStringNotEmpty()
   loginID: string;
+
+  @IsStringNotEmpty()
   password: string;
+
 }

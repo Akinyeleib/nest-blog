f05a850e14a2f0d1a8cf9437add0576e4917ef20
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-06-15 07:17:52 +0100

    Login username and password validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,22 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Body, Controller, ForbiddenException, Get } from '@nestjs/common';
 import { LoginDTO } from 'src/dto/login.dto';
 import { User } from 'src/entity/user.entity';
 import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcryptjs';
 
 @Controller('auth')
 export class AuthController {
   constructor(private userService: UserService) {}
   @Get('login')
   async login(@Body() loginDTO: LoginDTO): Promise<User> {
+    // check if user exists
     const user = await this.userService.getUserByUsername(loginDTO.loginID);
-    console.log(user);
+    // throw error if user does not exist
+    if (!user) throw new ForbiddenException('Invalid Credentials');
+    // validate password
+    const isValid = await bcrypt.compare(loginDTO.password, user.password);
+    // throw error for wrong password
+    if (!isValid) throw new ForbiddenException('Invalid Credentials');
     return user;
   }
 }

82fc42ef9bd6e02db8fc4fb15bff51b151382297
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-06-16 22:21:46 +0100

    basic blog inputs validation

--- src/dto/blog.dto.ts
+++ src/dto/blog.dto.ts
@@ -1,7 +1,16 @@
 /* eslint-disable prettier/prettier */
 
+import { IsNotEmpty, IsPositive } from "class-validator"
+import { IsStringNotEmpty } from "src/utils/helper"
+
 export class CreateBlogDTO {
+  @IsNotEmpty()
+  @IsPositive()
   author_id: number
+  
+  @IsStringNotEmpty()
   title: string
+  
+  @IsStringNotEmpty()
   content: string
 }

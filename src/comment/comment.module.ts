1f85627602e8b0466d5c485b0f199a9447963f20
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-06-17 09:16:23 +0100

    comment entity updated with Foreign Keys of User and Blog

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from 'src/user/user.module';
+import { Comments } from 'src/entity/comment.entity';
+import { BlogModule } from 'src/blog/blog.module';
 
 @Module({
   providers: [CommentService],
-  controllers: [CommentController]
+  controllers: [CommentController],
+  imports: [TypeOrmModule.forFeature([Comments]), BlogModule, UserModule],
 })
 export class CommentModule {}
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Comments } from 'src/entity/comment.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+  constructor(
+    @InjectRepository(Comments) private commentRepository: Repository<Comment>,
+  ) {}
+}

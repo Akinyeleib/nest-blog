1604ac018d19e256c61136af10de726239c87355
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-06-18 08:43:00 +0100

    Token genration Starts

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,10 +1,22 @@
 /* eslint-disable prettier/prettier */
 import { applyDecorators } from '@nestjs/common';
 import { IsNotEmpty, IsString } from "class-validator";
+import * as jwt from "jsonwebtoken"
+import { configDotenv } from 'dotenv';
+import { User } from 'src/entity/user.entity';
+configDotenv()
 
 export function IsStringNotEmpty() {
   return applyDecorators(
     IsString(),
     IsNotEmpty()
   );
+}
+
+export async function generateToken(user: User): Promise<string> {
+  const token = await jwt.sign({
+    id: user.id,
+    email: user.email,
+  }, process.env.JSON_TOKEN_KEY);
+  return token;
 }

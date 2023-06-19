3aefe2f53babe730b01859dfb6156c7476c8408b
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-06-19 19:48:39 +0100

    Generate token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,12 +3,13 @@ import { LoginDTO } from 'src/dto/login.dto';
 import { User } from 'src/entity/user.entity';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcryptjs';
+import { generateToken } from 'src/utils/helper';
 
 @Controller('auth')
 export class AuthController {
   constructor(private userService: UserService) {}
   @Get('login')
-  async login(@Body() loginDTO: LoginDTO): Promise<User> {
+  async login(@Body() loginDTO: LoginDTO): Promise<string> {
     // check if user exists
     const user = await this.userService.getUserByUsername(loginDTO.loginID);
     // throw error if user does not exist
@@ -17,6 +18,7 @@ export class AuthController {
     const isValid = await bcrypt.compare(loginDTO.password, user.password);
     // throw error for wrong password
     if (!isValid) throw new ForbiddenException('Invalid Credentials');
-    return user;
+
+    return generateToken(user);
   }
 }

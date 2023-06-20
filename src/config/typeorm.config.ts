635a10b29ab42f9de73647cbf1583e90dc89d194
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-06-20 14:21:41 +0100

    typeorm config dir

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,16 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Blog } from 'src/entity/blog.entity';
+import { Comments } from 'src/entity/comment.entity';
+import { User } from 'src/entity/user.entity';
+
+
+export const typeOrmConfig: TypeOrmModuleOptions = {  
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'nestblog',
+    entities: [User, Blog, Comments],
+    synchronize: true,
+  }

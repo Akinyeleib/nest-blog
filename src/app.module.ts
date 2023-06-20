ef76c98b3a2ef656efc887f5940115bedfb6533a
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-06-20 21:43:49 +0100

    typeorm config refactors

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogModule } from './blog/blog.module';
 import { UserModule } from './user/user.module';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { User } from './entity/user.entity';
-import { Blog } from './entity/blog.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { CommentModule } from './comment/comment.module';
-import { Comments } from './entity/comment.entity';
 import { typeOrmConfig } from './config/typeorm.config';
 
 

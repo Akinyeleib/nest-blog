981516fc3be103f023f3edc2454d2f662cb82ddd
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-06-20 14:19:13 +0100

    typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,30 +4,32 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogModule } from './blog/blog.module';
 import { UserModule } from './user/user.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
 import { Blog } from './entity/blog.entity';
 import { AuthModule } from './auth/auth.module';
 import { CommentModule } from './comment/comment.module';
 import { Comments } from './entity/comment.entity';
 
+
+
+const typeOrmConfig: TypeOrmModuleOptions = {  
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'root',
+  database: 'nestblog',
+  entities: [User, Blog, Comments],
+  synchronize: true,
+}
+
 @Module({
   imports: [
     BlogModule, 
     AuthModule, 
     UserModule,
-    TypeOrmModule.forRoot(
-      {
-        type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'root',
-        database: 'nestblog',
-        entities: [User, Blog, Comments],
-        synchronize: true,
-      }
-    ),
+    TypeOrmModule.forRoot(typeOrmConfig),
     CommentModule,
   ],
   controllers: [AppController],

ffb42f07463676cefb66b81dd6c66ace8d4c68db
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-06-25 23:03:34 +0100

    retrieve blog failed

--- src/middleware/blog.middleware.ts
+++ src/middleware/blog.middleware.ts
@@ -1,14 +1,20 @@
 import { NestMiddleware } from "@nestjs/common";
 import { NextFunction, Request, Response } from "express";
+import {BlogService}from '../blog/blog.service'
 
 export class BlogCheckMiddleware implements NestMiddleware {
     
-    use(req: Request, res: Response, next: NextFunction) {
+    constructor(private blogService: BlogService) {}
+
+    async use(req: Request, res: Response, next: NextFunction) {
         const blogID = req.baseUrl.split('/')[2]
         
         console.log(blogID);
- 
+        // const blog = await this.blogService.getBlog(Number(blogID))
+        // if (blog) next();
+
         next();
+        // else throw new 
     }
 
 }

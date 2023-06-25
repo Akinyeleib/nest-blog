a4ce408fa0d1dddc5e01fa395f0d3d9e3b8f51d4
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-06-25 22:52:37 +0100

    retrieve blod id from url

--- src/middleware/blog.middleware.ts
+++ src/middleware/blog.middleware.ts
@@ -4,12 +4,11 @@ import { NextFunction, Request, Response } from "express";
 export class BlogCheckMiddleware implements NestMiddleware {
     
     use(req: Request, res: Response, next: NextFunction) {
-        const id = req.params['id']
-        // console.log("ID is: " + id);
-        console.log(req.body);
+        const blogID = req.baseUrl.split('/')[2]
+        
+        console.log(blogID);
  
         next();
     }
-
 
 }

d2fcc67965a93562383ed1133c655b2c97add24c
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-06-25 22:29:25 +0100

    next() implementede

--- src/middleware/blog.middleware.ts
+++ src/middleware/blog.middleware.ts
@@ -4,7 +4,12 @@ import { NextFunction, Request, Response } from "express";
 export class BlogCheckMiddleware implements NestMiddleware {
     
     use(req: Request, res: Response, next: NextFunction) {
-        console.log("Hello from middleware...")
+        const id = req.params['id']
+        // console.log("ID is: " + id);
+        console.log(req.body);
+ 
+        next();
     }
+
 
 }

50727bed9be2ea7996e5f73c687f03296a95610e
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-06-25 21:58:50 +0100

    Blog Middleware test confirmed

--- src/middleware/blog.middleware.ts
+++ src/middleware/blog.middleware.ts
@@ -4,7 +4,7 @@ import { NextFunction, Request, Response } from "express";
 export class BlogCheckMiddleware implements NestMiddleware {
     
     use(req: Request, res: Response, next: NextFunction) {
-        throw new Error("Method not implemented.");
+        console.log("Hello from middleware...")
     }
 
 }

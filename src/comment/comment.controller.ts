f79c92703915d2b1f3afaa145dbf6119e8d87759
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-07-22 13:48:49 +0100

    retrieve token from header

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,9 +1,13 @@
-import { Body, Controller, ForbiddenException, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Body, Controller, ForbiddenException, Get, Headers, Param, ParseIntPipe, Post, Req, UnauthorizedException } from '@nestjs/common';
 import { Comments } from 'src/entity/comment.entity';
 import { CommentService } from './comment.service';
 import { CreateCommentDTO } from 'src/dto/comments.dto';
 import { UserService } from 'src/user/user.service';
 import { BlogService } from 'src/blog/blog.service';
+import * as jwt from "jsonwebtoken";
+import { configDotenv } from 'dotenv';
+import { Request } from 'express';
+configDotenv()
 
 @Controller('blogs/:id/comments')
 export class CommentController {
@@ -25,17 +29,29 @@ export class CommentController {
   }
 
   @Post()
-  async createComment(@Body() createCommentDTO: CreateCommentDTO): Promise<Comments> {
+  async createComment(@Req() req: any, @Headers() headers: any, @Body() createCommentDTO: CreateCommentDTO, @Param('id', ParseIntPipe) blogID: number) {
+    const token = headers.authorization
+    if (!token) {
+      throw new ForbiddenException();
+    } else {
+      const validToken: any = jwt.verify(token.split(" ")[1], process.env.JSON_TOKEN_KEY)
+      console.log(validToken)
+      req.currentUser = validToken?.id;
+      console.log(`current user: ${req.currentUser}`)
+    }
+
+    /*    
     // check if user exists
     const user = await this.userService.getUserByID(createCommentDTO.author_id);
     // throw error if user does not exist
     if (!user) throw new ForbiddenException('Invalid Author');
 
     // check if blog exists
-    const blog = await this.blogService.getBlog(createCommentDTO.blog);
+    const blog = await this.blogService.getBlog(blogID);
     // throw error if blog does not exist
     if (!blog) throw new ForbiddenException('Invalid Blog');
     return this.commentService.createComment(createCommentDTO, user, blog);
+*/
 
   }
 

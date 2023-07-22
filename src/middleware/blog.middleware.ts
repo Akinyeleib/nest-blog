1363d7c8c2e20f17f697ec94a24cdd20aea055ac
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-07-22 13:45:57 +0100

    blogService provider injected

--- src/middleware/blog.middleware.ts
+++ src/middleware/blog.middleware.ts
@@ -1,17 +1,18 @@
-import { BadRequestException, NestMiddleware } from "@nestjs/common";
-import { NextFunction, Request, Response } from "express";
-import {BlogService}from '../blog/blog.service'
+import { BadRequestException, Inject, Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import { BlogService } from '../blog/blog.service';
 
+@Injectable()
 export class BlogCheckMiddleware implements NestMiddleware {
+
+  constructor(@Inject(BlogService) private blogService: BlogService) {}
     
-    constructor(private blogService: BlogService) {}
 78
     async use(req: Request, res: Response, next: NextFunction) {
         const blogID = parseInt(req.params.id)
         console.log(`Blog is: ${blogID}, type is... ${typeof blogID}`)
         const blog = await this.blogService.getBlog(blogID)
         if (blog) {
-            console.log("Blog is present")
             next();
         }
         else throw new BadRequestException("Invalid blog specified")

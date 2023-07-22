ab7038c5cc0a6aa171b873ca7e60e192e6411415
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-07-22 13:32:01 +0100

    Blog ,iddleware improvements

--- src/middleware/blog.middleware.ts
+++ src/middleware/blog.middleware.ts
@@ -1,20 +1,20 @@
-import { NestMiddleware } from "@nestjs/common";
+import { BadRequestException, NestMiddleware } from "@nestjs/common";
 import { NextFunction, Request, Response } from "express";
 import {BlogService}from '../blog/blog.service'
 
 export class BlogCheckMiddleware implements NestMiddleware {
     
     constructor(private blogService: BlogService) {}
-
+78
     async use(req: Request, res: Response, next: NextFunction) {
-        const blogID = req.baseUrl.split('/')[2]
-        
-        console.log(blogID);
-        // const blog = await this.blogService.getBlog(Number(blogID))
-        // if (blog) next();
-
-        next();
-        // else throw new 
+        const blogID = parseInt(req.params.id)
+        console.log(`Blog is: ${blogID}, type is... ${typeof blogID}`)
+        const blog = await this.blogService.getBlog(blogID)
+        if (blog) {
+            console.log("Blog is present")
+            next();
+        }
+        else throw new BadRequestException("Invalid blog specified")
     }
 
 }
